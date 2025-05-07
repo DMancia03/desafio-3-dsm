de154f024784dbdbfdc6b514be8186bb345c6547
DMancia03 <diego.mancia.03@gmail.com>
2025-05-06 18:07:45 -0600

    Inicializando repo

---
+++ app/src/main/java/sv/edu/udb/colegiostone_recursos/MainActivity.kt
@@ -0,0 +1,47 @@
+package sv.edu.udb.colegiostone_recursos
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import sv.edu.udb.colegiostone_recursos.ui.theme.ColegioStoneRecursosTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ColegioStoneRecursosTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ColegioStoneRecursosTheme {
+        Greeting("Android")
+    }
+}
